56e5e8bf9e0d063b65f9f87285f8d78ce5bc6d41
ysbaek <ysbaekFox@gmail.com>
2021-12-24 11:36:06 +0900

    Logging Test
    Fomatter 사용한 Log Test

--- ysbaekFoxSite/polls/views.py
+++ ysbaekFoxSite/polls/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+import logging
 from django.http import HttpResponse
 
+logger = logging.getLogger('django')
+
 def index(request):
+    logger.error("test")
     return HttpResponse("Hello, world. You're at the polls index.")
